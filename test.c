c588b28f128b024295a8999bc8923a01dd891d44
Silverail <42062157+Silverail@users.noreply.github.com>
2020-12-30 11:46:32 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+//int global = 2020;
+//
+//void test()
+//{
+//	printf("global is %d\n", global);
+//}
+//
+//int main()
+//{
+//	test();
+//	int num = 0;
+//	{
+//		int num1 = 3;
+//	}
+//	printf("num = %d\n ", num);
+//	return 0;
+//}
+
+//{
+//	int i = 0;
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d%d", &num1, &num2);
+//	
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//} 
